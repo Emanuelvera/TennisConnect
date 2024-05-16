ab0f78adf29ae06c6d3dc1d54cd359c11d7a4543
camilamarinetto <camilamarinetto21@gmail.com>
2024-05-16 03:01:28 -0300

    Agregue todo el header

---
+++ cami/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var icono = document.getElementById("flecha");
+  
+    setTimeout(function() {
+      icono.style.opacity = "1";
+    }, 3000); 
+  });
+  
+  
