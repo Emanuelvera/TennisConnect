0f59a5ec82533eca9b8e53c42b388769f3afb66f
emanuel <emanuel.vera03>
2024-05-28 12:54:37 -0300

    added buscar equipos

--- script.js
+++ script.js
@@ -26,5 +26,26 @@ window.addEventListener("scroll", function() {
 
 
 
+// script.js
+
+
+var modal = document.getElementById("myModal");  // Obtener el modal
+var btn = document.getElementById("openModal");  // Obtener el botón que abre el modal
+var span = document.getElementsByClassName("close")[0]; // Obtener el elemento <span> que cierra el modal
+
+btn.onclick = function() {
+  modal.style.display = "block";  // Cuando el usuario hace clic en el botón, abre el modal
+}
+
+span.onclick = function() {
+  modal.style.display = "none";// Cuando el usuario hace clic en <span> (x), cierra el modal
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";// Cuando el usuario hace clic en cualquier lugar fuera del modal, lo cierra
+  }
+}
+
 
 
